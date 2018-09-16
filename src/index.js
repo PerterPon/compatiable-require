966b4f27fe33018036961d47260bc900f938f41f
Pon <PerterPon@gmail.com>
2018-09-16 21:07:49 +0800

    fix(main): fix main entry

---
+++ src/index.js
@@ -0,0 +1,25 @@
+
+/*
+  compatibleRequire
+  Author: PerterPon<PerterPon@gmail.com>
+  Create: Fri Sep 16 2018 15:48:20 GMT+0800
+*/
+
+const compatibleRequire = (path) => {
+
+    const requiredModule = require(path);
+    // TypeScript中模块需指定为commonjs，并且采用export default的方式导出。
+    let targetModule = null;
+
+    // babel和ts编译均会带上__esModule标志位
+    if (true === requiredModule.__esModule) {
+        targetModule = requiredModule.default;
+    } else {
+        targetModule = requiredModule;
+    }
+
+    return targetModule;
+
+}
+
+module.exports = compatibleRequire;
